5b0b7f6423abc623379f0a25f1090d519f7d8ca0
Joseph Murray <josephwmurray@gmail.com>
2023-02-10 17:33:13 -0800

    complete the project with pseudocode

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
-function ccValidator(number) {
-  let numNoSpace = number.replaceAll(" ", "");
-  let numArray = numNoSpace.split("");
-  let numArrayReverse = numArray.reverse();
-  numArrayReverse.unshift("0", "0");
-  let output = numArrayReverse.map(
-    (num, index) => (index % 2 === 0 ? num * 2 : num)
-    //                   condition   ?    true : false
-  );
-  output.splice(0, 2);
-
-  for (i = 0; i < output.length; i++) {
-    let newNum = output[i];
-    console.log(newNum);
-    if (newNum > 9) {
-      output = newNum[0] + newNum[1];
-      console.log(output);
-    } else {
-      output = newNum;
-      console.log(output);
+function ccMutator(cardNumber) {
+  //https://www.learnhowtoprogram.com/introduction-to-programming-part-time/arrays-and-looping-part-2/practice-credit-card-validator-roman-numerals-or-cryptosquare-part-1
+  
+  // args: cardNumber <String> (16 digits)
+  // return validationMessage: "This card number is not valid." | "This card number is valid."
+  // mutation algorithm:
+  // - double every other digit, right to left
+  // - for each doubled digit, if 2-digit, add together to a single digit number
+  // - sum all digits of the previous result
+  // - if that sum ends in 0, cardNumber is valid, otherwise invalid
+  
+  const numNoSpace = cardNumber.replaceAll(" ", "");
+  const numArray = numNoSpace.split();
+  const numArrayReverse = numArray.reverse();
+  const doubledArray = []; //doubled array
+  
+  for (i = 0; i < numArrayReverse.length; i++) {
+    let newNum = numArrayReverse[i];
+    if (i % 2 === 0) {
+      const doubled = Number(newNum) * 2;
+      if (doubled > 9) {
+        // add both digits of the 2 digit number together
+      }
+      // push doubled to doubledArray
     }
-  }
+ }
+  // grandTotal = add together all the numbers within doubledArray
+  // if grandTotal ends in 0, card is valid, otherwise invalid
 }
+  
